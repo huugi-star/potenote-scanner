5640d65e3388033ad2744ef526e2ee732542fb94
huugi-star <huugi-star@users.noreply.github.com>
2025-12-06 01:46:33 +0900

    修正

--- src/components/ui/DeveloperSupport.tsx
+++ src/components/ui/DeveloperSupport.tsx
@@ -10,7 +10,7 @@ import React from 'react';
 // リンク先（後で定数ファイルに移動してもOK）
 // TODO: 実際のnote記事URLとAmazonほしい物リストURLに差し替えてください
 const NOTE_URL = "https://note.com/aoi_potenote/n/n4b5b56006637; // ここにnote記事のURLを入れる
-const AMAZON_URL = "https://www.amazon.jp/hz/wishlist/ls/YOUR_LIST_ID"; // ここにほしい物リストのURLを入れる
+const AMAZON_URL = "https://www.amazon.jp/hz/wishlist/ls/216AXTCIGKTMN?ref_=wl_share"; // ここにほしい物リストのURLを入れる
 
 export const DeveloperSupport = () => {
   return (
