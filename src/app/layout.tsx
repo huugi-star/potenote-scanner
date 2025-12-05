576895783889296c4a91ebac5a99dd91b5233b44
huugi-star <huugi-star@users.noreply.github.com>
2025-12-05 22:53:01 +0900

    修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Potenote Scanner v2',
   description: '学習RPG - スキャンして学び、旅をしよう',
+  // ▼▼ サーチコンソールの確認コードはここに書くのが正解です ▼▼
+  verification: {
+    google: '-paAAzC_eZ8Eo4Oc8noUNf7heoGoXTtzPuyVvFU8a_E',
+  },
 };
 
 export default function RootLayout({
